0e640ca59d51e0feaf15528d1de26d5e8edb64ff
SamTessier <samcjtessier@gmail.com>
2024-10-29 03:38:02 -0230

    fix: update GoogleSheetsDataLayer to use correct imports and auth client

--- app/data/googlesheetsdatalayer.ts
+++ app/data/googlesheetsdatalayer.ts
@@ -1,13 +1,6 @@
 import { DataLayer } from "./datalayer";
 import { google } from 'googleapis';
-
-// Import the Google Sheets functions
-import { 
-  gsGetData, 
-  gsAddData, 
-  gsUpdateData, 
-  gsDeleteData 
-} from './googlesheetsfunctions';  // Create this file if it doesn't exist
+import { getData, addData, updateData, deleteData } from '../googlesheetsserver';
 
 class GoogleSheetsDataLayer implements DataLayer {
   private authClient: any;
@@ -55,24 +48,24 @@ class GoogleSheetsDataLayer implements DataLayer {
     }
   }
 
-  async getData() {
+  async getData(sheetName: string) {
     const auth = await this.authenticate();
-    return gsGetData(auth, this.spreadsheetId);
+    return getData(auth, this.spreadsheetId, sheetName);
   }
 
   async addData(data: any, sheetName: string) {
     const auth = await this.authenticate();
-    await gsAddData(auth, this.spreadsheetId, data, sheetName);
+    await addData(auth, this.spreadsheetId, data, sheetName);
   }
 
   async updateData(data: any, email: string, sheetName: string) {
     const auth = await this.authenticate();
-    await gsUpdateData(auth, this.spreadsheetId, data, sheetName, email);
+    await updateData(auth, this.spreadsheetId, data, sheetName, email);
   }
 
   async deleteData(email: string, sheetName: string) {
     const auth = await this.authenticate();
-    await gsDeleteData(auth, this.spreadsheetId, email, sheetName);
+    await deleteData(auth, this.spreadsheetId, email, sheetName);
   }
 }
 
